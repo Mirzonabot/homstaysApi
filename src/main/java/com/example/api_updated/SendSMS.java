521655b16457d80b66a28356f381f133fcc39099
Mirzonabot <mirzonabot.mirzonabotov_2023@ucentralasia.org>
2023-05-16 20:10:29 +0600

    updated for keys

--- src/main/java/com/example/api_updated/SendSMS.java
+++ src/main/java/com/example/api_updated/SendSMS.java
@@ -4,15 +4,15 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 
 public class SendSMS {
-    private static final String ACCOUNT_SID = "ACaae5abda9a0b61d9bc88a9a2c3130fdc";
+    private static final String ACCOUNT_SID = "SID";
 //    ACaae5abda9a0b61d9bc88a9a2c3130fdc
-    private static final String AUTH_TOKEN = "cfad8bf984319e8c4f1df3ea3bb155fc";
+    private static final String AUTH_TOKEN = "TOKEN";
 //    cfad8bf984319e8c4f1df3ea3bb155fc
     public static String sendSms(String phoneNumber, String message){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message1 = Message.creator(
                 new com.twilio.type.PhoneNumber(phoneNumber),
-                new com.twilio.type.PhoneNumber("+15076801067"),
+                new com.twilio.type.PhoneNumber("+###"),
                 message).create();
 
         return message1.getSid();
